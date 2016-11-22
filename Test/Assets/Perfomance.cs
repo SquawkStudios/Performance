5ed79aa843680a08037ecdb9bde0a493970b9d84
Jordan Ashton <jordie922@gmail.com>
2016-11-22 15:46:17 +0000

    Uploading for git purposes

--- Test/Assets/Perfomance.cs
+++ Test/Assets/Perfomance.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Perfomance : MonoBehaviour {
 	
+	//Sets the Maximum prime number the test can reach.
 	private int MaxPrime = 50;
-	
+	//initiates the current prime number the test is at. 
 	private int CurrentPrime;
 	
 	
 	// Use this for initialization
 	void Start () {
+		//Calls the date time of the system at start of test.
 		Debug.Log(System.DateTime.Now);           
 		
 		
@@ -20,6 +22,7 @@ public class Perfomance : MonoBehaviour {
 		PrimeNumber()  ;
 	}
 	
+	//This is the calculations for the prime number
 	public void PrimeNumber() {
 		for(int i = 2; i < MaxPrime; i++){
 			Debug.Log(i);
@@ -29,3 +32,8 @@ public class Perfomance : MonoBehaviour {
 		}
 	}
 }
+/*
+	Hi Rob. Im lost here. 
+	If you can point me in the right
+	direction would be appreciated
+*/
