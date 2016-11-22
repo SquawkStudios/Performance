bd27c670045f09a7411df93f35fc0d60e894006f
Jordan Ashton <jordie922@gmail.com>
2016-11-22 15:35:55 +0000

    Getting stuck

---
+++ Test/Assets/Perfomance.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Perfomance : MonoBehaviour {
+	
+	private int MaxPrime = 50;
+	
+	private int CurrentPrime;
+	
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log(System.DateTime.Now);           
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PrimeNumber()  ;
+	}
+	
+	public void PrimeNumber() {
+		for(int i = 2; i < MaxPrime; i++){
+			Debug.Log(i);
+			if(CurrentPrime == MaxPrime) {
+				Debug.Log(System.DateTime.Now);
+			}
+		}
+	}
+}
