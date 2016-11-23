21e4c3c0cee3cf450977b9a787337d199a2e1d68
Jordan Ashton <jordie922@gmail.com>
2016-11-23 10:30:06 +0000

    Created new Development branch, and fixed Math issue

--- Test/Assets/Perfomance.cs
+++ Test/Assets/Perfomance.cs
@@ -9,27 +9,57 @@ public class Perfomance : MonoBehaviour {
 	private int CurrentPrime;
 	
 	
+	
+	
 	// Use this for initialization
 	void Start () {
 		//Calls the date time of the system at start of test.
-		Debug.Log(System.DateTime.Now);           
+		Debug.Log(DateTime());           
 		
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		PrimeNumber()  ;
+		CalculatePrimeNumber()  ;
 	}
 	
 	//This is the calculations for the prime number
 	public void PrimeNumber() {
 		for(int i = 1; i < MaxPrime; i++){
-			Debug.Log(i);                               // Call method which calculates if our number is a prime, our number being i
+			Debug.Log(i);   // Call method which calculates if our number is a prime, our number being i
 			if(CurrentPrime == MaxPrime) {
-				Debug.Log(System.DateTime.Now);
+				Debug.Log(DateTime());
 			}
 		}
+	}
+	
+	private void CalculatePrimeNumber(int numberToTest)
+	{
+		if (numberToTest == 2) Debug.Log("Prime: " + numberToTest);
+		
+		if (numberToTest > 2)
+		{
+			bool isPrime = true;
+			
+			for (int j = 2; j <= (int)Mathf.Floor(Mathf.Sqrt(numberToTest)); ++j)
+			{
+				if (numberToTest % j == 0)
+				{
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if (isPrime == true)
+			{
+				Debug.Log("Prime: " + numberToTest);
+			}
+		}
+	}
+	
+	private void DateTime(){
+		System.DateTime.Now;
 	}
 }
 /*
