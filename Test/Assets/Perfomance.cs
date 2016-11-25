4faa7808f92bc147dbf0bd3cf1a0798adc78bfe0
Jordan Ashton <jordie922@gmail.com>
2016-11-25 12:45:38 +0000

    Updated MaxPrime variable to 500000

--- Test/Assets/Perfomance.cs
+++ Test/Assets/Perfomance.cs
@@ -1,21 +1,15 @@
 ﻿using UnityEngine;
-					// TODO: Not in use, added automatically by the template MonoDevelop uses when you create a Script within Unity, could be removed
 
 public class Perfomance : MonoBehaviour {
 	
 	//Sets the Maximum prime number the test can reach.
-	private int MaxPrime = 50;
-	//initiates the current prime number the test is at. 
-	//private int CurrentPrime;					// TODO: This is now redundant (see below)
-	
-	
-	
+	private int MaxPrime = 500000;
 	
 	// Use this for initialization
 	void Start () {
 		//Calls the date time of the system at start of test.
 		//Debug.Log(DateTime());           
-		DateTime();		// TODO: Your DateTime() method looks good, call that here instead, e.g. comment this line out completely and add DateTime(); below it
+		DateTime();	
 		PrimeNumber();
 		
 	}
@@ -23,13 +17,13 @@ public class Perfomance : MonoBehaviour {
 	//This is the calculations for the prime number
 	public void PrimeNumber() {
 		for(int i = 1; i <= MaxPrime; i++){
-			CalculatePrimeNumber(i);   // Call method which calculates if our number is a prime, our number being is
+			// Call method which calculates if our number is a prime, our number being is
+			CalculatePrimeNumber(i);
 		}
 		DateTime();
-		// TODO: This would be a good spot to call your DateTime() method again as it will display the date/time once the iterations have completed
 	}
 	
-	public void CalculatePrimeNumber(int numberToTest)
+	private void CalculatePrimeNumber(int numberToTest)
 	{
 		if (numberToTest == 2) Debug.Log("Prime: " + numberToTest);
 		
